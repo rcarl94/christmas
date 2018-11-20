8710df0b6285a419d78a97ee55e21a9c09044390
Ryan Carl <rcarl94@gmail.com>
2018-11-20 00:27:45 -0600

    initial commit

---
+++ src/main/java/carl/ryan/christmas/ChristmasApplication.java
@@ -0,0 +1,12 @@
+package carl.ryan.christmas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristmasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChristmasApplication.class, args);
+	}
+}
